7dd04eaf17c77aaa8a8089378b5544423029ce76
wenh <wenh>
2024-12-25 01:17:48 +0800

    Initial Commit

---
+++ activity-card/activity_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  activity_cardApp.swift
+//  activity-card
+//
+//  Created by 活点地图 on 2024/12/25.
+//
+
+import SwiftUI
+
+@main
+struct activity_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ activity-cardTests/activity_cardTests.swift
@@ -0,0 +1,17 @@
+//
+//  activity_cardTests.swift
+//  activity-cardTests
+//
+//  Created by 活点地图 on 2024/12/25.
+//
+
+import Testing
+@testable import activity_card
+
+struct activity_cardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
